73162575806976d731a35dc5071b14e98535567d
powiedl <159603185+powiedl@users.noreply.github.com>
2024-05-23 05:01:56 +0200

    Code Challenge - meine Lösung

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(100);
+  const [yourPercent, setYourPercent] = useState(5);
+  const [friendPercent, setFriendPercent] = useState(5);
+
+  function Reset() {
+    setBill(100);
+    setYourPercent(0);
+    setFriendPercent(0);
+  }
+
+  const yourTip = (bill * yourPercent) / 100;
+  const friendTip = (bill * friendPercent) / 100;
+  const tip = (yourTip + friendTip) / 2;
+  const total = bill + yourTip + friendTip;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bill bill={bill} onChange={setBill} />
+      <ChoosePercent value={yourPercent} onChange={setYourPercent}>
+        How did you like the service?
+      </ChoosePercent>
+      <ChoosePercent value={friendPercent} onChange={setFriendPercent}>
+        How did your friend like the service?
+      </ChoosePercent>
+      <ShowTip>
+        You pay ${total} ( ${Number(bill)} + ${tip} tip)
+      </ShowTip>
+      <ResetButton onClick={Reset}>Reset</ResetButton>
     </div>
   );
+}
+
+function Bill({ bill, onChange }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input value={bill} onChange={(e) => onChange(Number(e.target.value))} />
+    </div>
+  );
+}
+
+function ChoosePercent({ onChange, value, children }) {
+  return (
+    <div>
+      {children}
+      <select value={value} onChange={(e) => onChange(Number(e.target.value))}>
+        <option value={0}>Bad (0%)</option>
+        <option value={5}>Ok (5%)</option>
+        <option value={10}>Good (10%)</option>
+        <option value={20}>Amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function ShowTip({ children }) {
+  return <h1>{children}</h1>;
+}
+
+function ResetButton({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
 
 export default App;
